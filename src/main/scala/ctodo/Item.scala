fc1df6296221eb5657439f602daa7c2509080a1a
2022_050227-ChenXiuzhuang <chenrx_sky@163.com>
2024-09-22 17:38:38 +0800

    finish show function

--- src/main/scala/ctodo/Item.scala
+++ src/main/scala/ctodo/Item.scala
@@ -1,7 +1,17 @@
 package ctodo
 object log_path{
-    val log_path:os.Path=os.pwd
+    val log_path:os.Path=os.pwd/"misc"
 }
 sealed abstract trait Item
 case class todo(thing:String) extends Item
 case class done(thing:String) extends Item
+
+object Itemtoolkit{
+    def show(item:Item):String ={
+        item match {
+            case todo(thing) => "[todo] " + thing
+            case done(thing) => "[done] " + thing
+        }
+    }
+    /*def codetoFile(list:Array[Item]):Unit*/
+}
--- src/test/scala/ctodo/ItemTests.scala
+++ src/test/scala/ctodo/ItemTests.scala
@@ -2,7 +2,10 @@ package ctodo
 import org.scalatest.funsuite.AnyFunSuite
 
 class ItemSuite extends AnyFunSuite{
-    test("log_path"){
-        assert(true)
+    test("show in item tool kit"){
+        assert(Itemtoolkit.show(todo("123"))=="[todo] 123")
+        assert(Itemtoolkit.show(done("567"))=="[done] 567")
+    }
+    test("write list to file"){
     }
 }
