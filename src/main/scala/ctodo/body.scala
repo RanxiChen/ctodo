dfa85aec68f64d011d7fee86be4bb3faf9ed1bfd
chenxiuzhuang <chenxiuzhuang_2002@163.com>
2024-12-15 18:22:38 +0800

    stop a few times to clearify what can this app do

--- src/main/scala/ctodo/body.scala
+++ src/main/scala/ctodo/body.scala
@@ -2,4 +2,7 @@ package ctodo
 
 object ctodo extends App{
     println("Hello, wellcome to ctodo")
+    var taskspool=FileandTask.getTasks()
+    taskspool= CURD.create("test what can this do,now",taskspool)
+    taskspool.map(Tasktoolkit.show).map(println)
 }
